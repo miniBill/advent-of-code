faabbdf0e0109ec591845184aff2d49d5f80d59f
Leonardo Taglialegne <leonardo.taglialegne@rstor.io>
2021-12-01 12:23:57 +0100

    Day1

--- src/Day1.elm
+++ src/Day1.elm
@@ -1,11 +1,52 @@
 module Day1 exposing (process, processGold)
 
 
+parse : List String -> List Int
+parse lines =
+    List.filterMap String.toInt lines
+
+
 process : List String -> String
 process lines =
-    List.length lines |> String.fromInt
+    case parse lines of
+        [] ->
+            "Invalid input"
+
+        h :: t ->
+            t
+                |> List.foldl
+                    (\e ( count, last ) ->
+                        ( if e > last then
+                            count + 1
+
+                          else
+                            count
+                        , e
+                        )
+                    )
+                    ( 0, h )
+                |> Tuple.first
+                |> String.fromInt
 
 
 processGold : List String -> String
 processGold lines =
-    List.length lines |> String.fromInt
+    case parse lines of
+        ha :: hb :: hc :: t ->
+            t
+                |> List.foldl
+                    (\e ( count, ( a, b, c ) ) ->
+                        ( if b + c + e > a + b + c then
+                            count + 1
+
+                          else
+                            count
+                        , ( b, c, e )
+                        )
+                    )
+                    ( 0, ( ha, hb, hc ) )
+                |> Tuple.first
+                |> String.fromInt
+
+        _ ->
+            "Invalid input"
