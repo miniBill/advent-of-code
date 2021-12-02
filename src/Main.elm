e4bb19e9ae05255e28d85a19a8cf9fcc80814cd0
Leonardo Taglialegne <leonardo.taglialegne@rstor.io>
2021-12-02 18:51:56 +0100

    Day 6 - 2020

--- src/Main.elm
+++ src/Main.elm
@@ -18,6 +18,7 @@ import Y2020.Day2
 import Y2020.Day3
 import Y2020.Day4
 import Y2020.Day5
+import Y2020.Day6
 import Y2021.Day1
 import Y2021.Day2
 
@@ -70,6 +71,7 @@ view model =
                 , dayI Y2020.Day3.parser Y2020.Day3.process Y2020.Day3.processGold
                 , dayI Y2020.Day4.parser Y2020.Day4.process Y2020.Day4.processGold
                 , dayI Y2020.Day5.parser Y2020.Day5.process Y2020.Day5.processGold
+                , dayI Y2020.Day6.parser Y2020.Day6.process Y2020.Day6.processGold
                 ]
               )
             , ( 2021
---
+++ src/Y2020/Day6.elm
@@ -0,0 +1,47 @@
+module Y2020.Day6 exposing (parser, process, processGold)
+
+import List.Extra
+import Parser exposing ((|.), (|=), Parser)
+import Set exposing (Set)
+
+
+type alias Item =
+    Set Char
+
+
+parser : Parser Item
+parser =
+    Parser.chompWhile (always True)
+        |> Parser.getChompedString
+        |> Parser.map (String.toList >> Set.fromList)
+
+
+aggregate : List Item -> List ( Item, List Item )
+aggregate list =
+    list
+        |> List.Extra.groupWhile (\a b -> Set.isEmpty a == Set.isEmpty b)
+        |> List.Extra.filterNot (\( h, _ ) -> Set.isEmpty h)
+
+
+process : List Item -> Int
+process list =
+    list
+        |> aggregate
+        |> List.map
+            (\( h, t ) ->
+                List.foldr Set.union h t
+                    |> Set.size
+            )
+        |> List.sum
+
+
+processGold : List Item -> Int
+processGold list =
+    list
+        |> aggregate
+        |> List.map
+            (\( h, t ) ->
+                List.foldr Set.intersect h t
+                    |> Set.size
+            )
+        |> List.sum
