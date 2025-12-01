9c562451499d8810cc6140c2162838884e1102cd
Leonardo Taglialegne <leonardo@taglialegne.it>
2025-12-01 11:51:00 +0100

    Preparing for part 2

--- 2025/src/bin/day1.rs
+++ 2025/src/bin/day1.rs
@@ -3,7 +3,8 @@ fn main() {
         std::fs::read_to_string("inputs/day1.txt").expect("Failed to read the input file");
     let lines = input.split("\n").filter(|l| !l.is_empty());
     let mut pointing_at = 50i32;
-    let mut zeroes = 0u32;
+    let mut part1 = 0u32;
+    let mut part2 = 0u32;
     for line in lines {
         let direction: Direction = if line.starts_with("L") {
             Direction::Left
@@ -21,10 +22,11 @@ fn main() {
             Direction::Right => (pointing_at + number) % 100,
         };
         if pointing_at == 0 {
-            zeroes += 1;
+            part1 += 1;
         }
     }
-    println!("Zeroes: {}", zeroes)
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 enum Direction {
